9bce7aca8c849c63d2820ea93d2177dc599e9939
Jessica Filippelli <j_fil@cslab-green.concordia.ca>
2019-10-22 13:00:57 -0400

    images for moke ups

--- dart349/majorprojects/midterm/Mockups/imagepopup.js
+++ dart349/majorprojects/midterm/Mockups/imagepopup.js
@@ -1,21 +1,27 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+window.onload  = function(){
+  console.log("loaded");
+
+let modal = document.getElementById("myModal");
+let captionText = document.getElementById("caption");
+let span = document.getElementsByClassName("close")[0];
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+let imgs = document.getElementsByClassName("myImg");
+let modalImg = document.getElementById("img01");
+for(let i =0; i< imgs.length;i++){
+  //console.log(imgs[i]);
+  imgs[i].onclick = function(){
+    modal.style.display = "block";
+   modalImg.src = this.src;
+   captionText.innerHTML = this.alt;
+    console.log("clicked");
+  }
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+ modal.style.display = "none";
+}
+
+
 }
